8e64f53f4a1fb76e81d87c72d9f1273dbd2091b3
arlin99potato <aliabintirazali@proton.me>
2024-08-27 18:22:50 +0800

    Another

---
+++ Tutorial/src/algorithm_data_structure/RemoveEvenIntegerArray.java
@@ -0,0 +1,47 @@
+package algorithm_data_structure;
+
+public class RemoveEvenIntegerArray {
+	
+	public static void main(String[] args) {
+		// Create instance due to reside inside static method
+		RemoveEvenIntegerArray object = new RemoveEvenIntegerArray();
+		
+		int[] array = {3,2,4,7,10,6,5};
+		object.removeEvenInteger(array);
+	}
+	
+	public void removeEvenInteger(int[] array) {
+		int count = 0;
+		
+		System.out.print("Array: { ");
+		
+		// Count the number of even integers
+		// To create new array
+		for(int i = 0; i < array.length-1; i++) {
+			System.out.print(array[i] + " ");
+			
+			// If element is not divisible by 0
+			if(array[i] % 2 != 0) {
+				count++;
+			}
+		}
+		
+		System.out.println("}");
+		
+		// Create new array with default value
+		int[] newArray = new int[count];
+		int newCount = 0;
+		
+		System.out.print("New Array: { ");
+		
+		for(int i = 0; i < array.length-1; i++) {
+			if(array[i] % 2 != 0) {
+				newArray[newCount] = array[i];
+				System.out.print(newArray[newCount] + " ");
+				newCount++;
+			}
+		}
+		
+		System.out.print("}");
+	}
+}
